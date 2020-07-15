1936f3a7d5ea299502e0c84d3574122c2dae6970
Gruselhaus <nico@gruselhaus.com>
2020-07-15 13:01:32 +0200

    User input for miau command

--- src/commands/fun/miau.js
+++ src/commands/fun/miau.js
@@ -1,5 +1,6 @@
-exports.run = async (bot, msg) => {
-  msg.channel.send(`https://cataas.com/cat/says/miau?${new Date().getTime()}`);
+exports.run = async (bot, msg, args) => {
+  const text = args.length < 1 ? "miau" : args[0];
+  msg.channel.send(`https://cataas.com/cat/says/${text}?${new Date().getTime()}`);
 };
 
 exports.info = {
