6336fb91fedebecc0990eab10375ff927bf7fae4
Gruselhaus <nico@gruselhaus.com>
2020-07-15 13:07:18 +0200

    Fixing bug in miau command

--- src/commands/fun/miau.js
+++ src/commands/fun/miau.js
@@ -1,9 +1,11 @@
 exports.run = async (bot, msg, args) => {
-  const text = args.length < 1 ? "miau" : args[0];
-  msg.channel.send(`https://cataas.com/cat/says/${text}?${new Date().getTime()}`);
+  const text = args.length < 1 ? "miau" : args;
+  msg.channel.send(`https://cataas.com/cat/says/${text.join("%20")}?${new Date().getTime()}&color=white&size=50&type=or`);
 };
 
-exports.info = {
+//cataas.com/cat/says/Hello?&color=white&size=50&type=or
+
+https: exports.info = {
   name: "miau",
   usage: "miau",
   help: "Returns a random cat image",
