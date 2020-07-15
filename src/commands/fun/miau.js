09f0a809b68c6ad5a68382f0d66b036dcbcdb9d6
Gruselhaus <nico@gruselhaus.com>
2020-07-15 13:35:36 +0200

    Fixing different cat bug

--- src/commands/fun/miau.js
+++ src/commands/fun/miau.js
@@ -1,11 +1,13 @@
+const fetch = require("node-fetch");
+
 exports.run = async (bot, msg, args) => {
   const text = args.length < 1 ? "miau" : args;
-  msg.channel.send(`https://cataas.com/cat/says/${text.join("%20")}?${new Date().getTime()}&color=white&size=50&type=or`);
+  fetch(`https://cataas.com/cat/says/${text.join("%20")}?${new Date().getTime()}&color=white&size=50&type=or`)
+    .then((res) => res.buffer())
+    .then((buffer) => msg.channel.send("", { files: [buffer] }));
 };
 
-//cataas.com/cat/says/Hello?&color=white&size=50&type=or
-
-https: exports.info = {
+exports.info = {
   name: "miau",
   usage: "miau",
   help: "Returns a random cat image",
