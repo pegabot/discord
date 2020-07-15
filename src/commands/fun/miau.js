3b61185d64af4ceb7a0da25fcf6648f24c883fc9
Gruselhaus <nico@gruselhaus.com>
2020-07-15 13:39:09 +0200

    fixing array bug

--- src/commands/fun/miau.js
+++ src/commands/fun/miau.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 exports.run = async (bot, msg, args) => {
-  const text = args.length < 1 ? "miau" : args;
+  const text = args.length < 1 ? ["miau"] : args;
   fetch(`https://cataas.com/cat/says/${text.join("%20")}?${new Date().getTime()}&color=white&size=50&type=or`)
     .then((res) => res.buffer())
     .then((buffer) => msg.channel.send("", { files: [buffer] }));
