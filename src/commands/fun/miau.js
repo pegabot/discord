772894343784e8ab0ddb6c548361da98f7147b69
Gruselhaus <nico@gruselhaus.com>
2020-07-16 13:43:29 +0200

    Berücksichtigungen der Benutzernamen in miau.js

--- src/commands/fun/miau.js
+++ src/commands/fun/miau.js
@@ -4,7 +4,13 @@ const querystring = require("querystring");
 const { MessageAttachment } = require("discord.js");
 
 exports.run = async (bot, msg, args) => {
-  const text = args.length < 1 ? ["miau"] : args;
+  const text =
+    args.length < 1
+      ? ["miau"]
+      : msg.cleanContent
+          .slice(bot.config.prefix.length + 4)
+          .trim()
+          .split(" ");
   fetch(`https://cataas.com/cat/says/${querystring.escape(emojiStrip(text.join(" ")))}?${new Date().getTime()}&size=50&color=white&type=large`)
     .then((res) => res.buffer())
     .then((buffer) => {
