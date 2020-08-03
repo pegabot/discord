6681b938009c650476a36766f1e9d94381861ab8
Gruselhaus <nico@gruselhaus.com>
2020-08-03 11:16:01 +0200

    Updating guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -5,21 +5,18 @@ exports.run = (bot, member) => {
   //   stripIndents(`
   //       Wilkommen an Bord ${member}!`),
   // );
-  bot.users.cache.get(member.user.id).send(
-    stripIndents(`
-        Willkommen auf CONspiracy,
-        
-        ich freue mich dich hier begrüßen zu dürfen. 
-        Die nächste CONspirancy findet vom 17.07-19.07.2020 statt.
-        Wenn Du dich für Rollenspielrunden interessierst, findest du alle ausgeschriebenen Runden hier: 
-        https://trello.com/b/DQW3k460/pegasus-spiele-conspiracy
 
-        Wenn Du für Gespräche unter Gleichgesinnten hier bist, unterhalte dich gern mit uns im #small-talk Channel. 
+  bot.channels.resolve(bot.config.modLog).send(stripIndents(`${member} hat gerade den Server betreten!`));
 
-        Ich wünsche dir viel Spaß, 
-        
-        Dein Pegabot`),
-  );
+  // bot.users.cache.get(member.user.id).send(
+  //   stripIndents(`
+  //       Willkommen auf CONspiracy,
+  //       ich freue mich dich hier begrüßen zu dürfen.
+  //       Die nächste CONspirancy findet vom 17.07-19.07.2020 statt.
+  //       Wenn Du dich für Rollenspielrunden interessierst, findest du alle ausgeschriebenen Runden hier:
+  //       https://trello.com/b/DQW3k460/pegasus-spiele-conspiracy
+  //       Wenn Du für Gespräche unter Gleichgesinnten hier bist, unterhalte dich gern mit uns im #small-talk Channel.
+  //       Ich wünsche dir viel Spaß,
+  //       Dein Pegabot`),
+  // );
 };
-
-exports.disable = true;
