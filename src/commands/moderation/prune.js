cf689f429beeef86569cf4bb813c1a3adc148852
Gruselhaus <nico@gruselhaus.com>
2020-10-18 15:54:48 +0200

    Updating the prune command

--- src/commands/moderation/prune.js
+++ src/commands/moderation/prune.js
@@ -7,17 +7,9 @@ const { BotExecption } = require("../../utils");
 exports.run = async (bot, msg, args) => {
   if (args.length < 1) throw new BotExecption("Bitte übergeben die Anzahl der zu löschenden Nachrichten.");
   if (Number.isNaN(args[0])) throw new BotExecption("Deine Anzahl ist keine Zahl.");
-  if (args[0] > 50) throw new BotExecption("Ich kann nicht mehr als 50 Nachrichten auf Einmal löschen.");
+  if (args[0] > 99) throw new BotExecption("Ich kann nicht mehr als 88 Nachrichten auf Einmal löschen.");
 
-  const messages = await msg.channel.messages.fetch({ limit: Number(args[0]) + 1 });
-
-  for (const msgToDelete of messages.values()) {
-    if (msgToDelete.deletable) {
-      msgToDelete.delete();
-    } else {
-      msg.channel.send(`Ich konnte die folgende Nachricht nicht löschen\n>>> ${msgToDelete.content}`);
-    }
-  }
+  await msg.channel.bulkDelete(Number(args[0]) + 1);
 };
 
 exports.info = {
