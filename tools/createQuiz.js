3d1cce7fa2c9eef7e4686b76e46f3e3b00864e06
Gruselhaus <nico@gruselhaus.com>
2020-10-18 15:38:33 +0200

    Updaing quiz generation tool

--- tools/createQuiz.js
+++ tools/createQuiz.js
@@ -11,18 +11,29 @@ const mongoose = require("mongoose");
 const { schema: QuizSchema } = require("../src/models/quiz");
 
 const filename = "quiz.json";
-const QuizName = "SPIEL.digital";
 
 (async () => {
   if (!fs.existsSync(path.join(__dirname, filename))) throw new Error(`${filename} existiert nicht!`);
-  const questions = require(path.join(__dirname, filename));
+  const quizzes = require(path.join(__dirname, filename));
 
   await mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, autoIndex: true, useFindAndModify: false });
   const QuizModel = mongoose.model("quiz", QuizSchema);
 
-  const ModelQuestion = new QuizModel();
-  ModelQuestion.name = QuizName;
-  ModelQuestion.fragen = questions;
-  await ModelQuestion.save();
+  for (const quiz of quizzes) {
+    const newQuiz = new QuizModel();
+    newQuiz.name = quiz.name;
+    newQuiz.fragen = quiz.fragen;
+
+    const existingQuiz = await QuizModel.find({ name: newQuiz.name });
+    if (existingQuiz.length !== 0) {
+      existingQuiz.forEach(async (elt) => {
+        console.log(`${elt.name} exists - deleting!`);
+        await elt.remove();
+      });
+    }
+    console.log(`Saving ${newQuiz.name}!`);
+    await newQuiz.save();
+  }
+
   process.exit(0);
 })();
