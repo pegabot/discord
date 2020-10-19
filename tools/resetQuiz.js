f07a4b4d8610179e49e9822fc48c0bdcb5ee05e2
Gruselhaus <nico@gruselhaus.com>
2020-10-19 23:42:55 +0200

    Adding script to reset quiz at once

---
+++ tools/resetQuiz.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Pegasus Spiele Verlags- und Medienvertriebsgesellschaft mbH, all rights reserved.
+ */
+
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const { schema: SessionSchema } = require("../src/models/session");
+const { schema: QuizSchema } = require("../src/models/quiz");
+const { schema: VoucherSchema } = require("../src/models/voucher");
+
+const { exec } = require("child_process");
+
+(async () => {
+  await mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, autoIndex: true, useFindAndModify: false });
+
+  const SessionModel = mongoose.model("session", SessionSchema);
+  const QuizModel = mongoose.model("quiz", QuizSchema);
+  const VoucherModel = mongoose.model("voucher", VoucherSchema);
+
+  const existingSessions = await SessionModel.find({});
+  if (existingSessions.length !== 0) {
+    existingSessions.forEach(async (elt) => {
+      console.log(`Deleting Session with id ${elt._id}!`);
+      await elt.remove();
+    });
+  }
+
+  const existingQuizzes = await QuizModel.find({});
+  if (existingQuizzes.length !== 0) {
+    existingQuizzes.forEach(async (elt) => {
+      console.log(`Deleting Quiz with id ${elt._id}!`);
+      await elt.remove();
+    });
+  }
+
+  const existingVouchers = await VoucherModel.find({});
+  if (existingVouchers.length !== 0) {
+    existingVouchers.forEach(async (elt) => {
+      console.log(`Deleting Voucher with id ${elt._id}!`);
+      await elt.remove();
+    });
+  }
+
+  exec("yarn createQuiz");
+  exec("yarn createVouchers");
+
+  process.exit(0);
+})();
