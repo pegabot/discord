61d34b60cc2aa5b8f18168a6e7bc7f75ae27049f
Gruselhaus <nico@gruselhaus.com>
2020-11-25 21:51:05 +0100

    Final text updates

--- src/functions/vouchers.js
+++ src/functions/vouchers.js
@@ -25,11 +25,11 @@ exports.run = async (bot) => {
 
       ***Lege dazu den Bundle-Artikel (https://www.pegasusdigital.de/product/337316/CONspiracy-Bundle-BUNDLE) in den Warenkorb und löse danach den Gutscheincode ein. Die Kosten für das Bundle werden dir dann im Warenkorb erstattet.***
 
-      Ebenfalls auf www.pegasusdigital.de erhältst du von 27.-29.11.2020 außerdem 20% Rabatt auf Artikel, die innerhalb der letzten zwei Jahre erschienen sind und sogar 40% Rabatt auf Artikel, die schon älter als zwei Jahre sind (Oldies but Goldies!). Bundles und Neuheiten, die innerhalb der letzten 30 Tage erschienen sind, sind von der Rabattaktion ausgeschlossen.
+      Ebenfalls auf www.pegasusdigital.de erhältst du von 26.-30.11.2020 außerdem 20% Rabatt auf Artikel, die innerhalb der letzten zwei Jahre erschienen sind und sogar 40% Rabatt auf Artikel, die schon älter als zwei Jahre sind (Oldies but Goldies!). Neuheiten, die innerhalb der letzten 30 Tage erschienen sind, und Bundles sind von der Rabattaktion ausgeschlossen.
 
-      Außerdem kannst du noch bis 29.11.2020 23:59 Uhr an unserer CONspiracy-Umfrage teilnehmen: https://de.surveymonkey.com/r/QK9YDD6 Unter allen, die uns dort ihre Meinung sagen, verlosen wir drei Überraschungspakete im Wert von mindestens 100€.
+      Außerdem kannst du noch bis 29.11.2020 23:59 Uhr an unserer CONspiracy-Umfrage teilnehmen: https://de.surveymonkey.com/r/QK9YDD6. Unter allen, die uns dort ihre Meinung sagen, verlosen wir drei Überraschungspakete im Wert von mindestens 100€.
 
-      Und schließlich, falls du regelmäßig die neuesten Updates zu unseren Events, Aktionen und Angeboten erhalten möchtest, dann abonniere unseren allgemeinen Newsletter unter www.pegasus.de/newsletter. In deinem Pegasus Digital-Konto kannst du dich außerdem für unseren Rollenspiel-Newsletter anmelden. Und wer weiß, vielleicht lässt sich dort auch das ein oder andere Wissen für unser nächstes Geek-Quiz sammeln!
+      Und schließlich, falls du regelmäßig die neuesten Updates zu unseren Events, Aktionen und Angeboten erhalten möchtest, dann abonniere unseren allgemeinen Pegasus Spiele-Newsletter unter www.pegasus.de/newsletter. In deinem Pegasus Digital-Konto kannst du dich außerdem für unseren Rollenspiel-Newsletter anmelden. Und wer weiß, vielleicht lässt sich dort auch das ein oder andere Wissen für unser nächstes Geek-Quiz sammeln!
 
       Dein Pegabot :robot:
       `),
