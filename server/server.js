ad4f0f665b4ab4cc0de0446f1a0e4bfdf861d98c
Gruselhaus <nico@gruselhaus.com>
2020-12-23 23:49:24 +0100

    Adding 60px x 60px Logo to webserver

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@
  */
 
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const server = express();
 
 server.get("/", (_, res) => {
   res.redirect(process.env.inviteUrl || "https://pegasus.de");
 });
+
+server.use(express.static(path.resolve(__dirname, "static")));
 
 server.listen(process.env.PORT || 80, () => console.log("Webserver started!"));
