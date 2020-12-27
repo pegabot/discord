4687de631b23337273f1c559a15b43c836b94b83
Gruselhaus <nico@gruselhaus.com>
2020-12-27 16:55:35 +0100

    Adding blogs from Pegasus US

--- src/functions/blog.js
+++ src/functions/blog.js
@@ -10,13 +10,13 @@ const {
 exports.run = async (bot) => {
   let {
     data: { data: entries },
-  } = await getRequest(bot, `blog/?limit=9999999&filter[0][property]=categoryId&filter[0][expression]=IN&filter[0][value][0]=115&filter[0][value][1]=560`);
+  } = await getRequest(bot, `blog/?limit=9999999&filter[0][property]=categoryId&filter[0][expression]=IN&filter[0][value][0]=115&filter[0][value][1]=560&filter[0][value][2]=589&filter[0][value][3]=713`);
 
   const BlogModel = bot.db.model("blog");
 
   const current_blogPosts = await BlogModel.find({});
 
-  entries = entries.filter((elt) => elt.active && !current_blogPosts.map((elt) => elt.blogPost_id).includes(elt.id.toString()));
+  entries = entries.filter((elt) => new Date() > new Date(elt.displayDate) && elt.active && !current_blogPosts.map((elt) => elt.blogPost_id).includes(elt.id.toString()));
 
   for (const entry of entries) {
     try {
@@ -25,22 +25,34 @@ exports.run = async (bot) => {
         title,
       } = entry;
 
-      let url, message;
+      let channel, url, message;
       switch (catId) {
         case 115:
+          channel = bot.config.BLOG_CHANNEL_DE;
           url = `https://pegasus.de/presse/pressemitteilungen/${slugify(title)}`;
           message = `Unsere Pressestelle hat eben gerade eine neue Mitteilung veröffentlicht! 📣 ${url}`;
           break;
         case 560:
+          channel = bot.config.BLOG_CHANNEL_DE;
           url = `https://pegasus.de/news/pegasus-spiele-blog/${slugify(title)}`;
           message = `Auf unserem Blog ist gerade ein neuer Beitrag erschienen 📄 ${url}`;
+          break;
+        case 589:
+          channel = bot.config.BLOG_CHANNEL_EN;
+          url = `https://pegasus-web.com/news/${slugify(title)}`;
+          message = `Our US-Team released a new article in our new room 📣  Check it out!${url}`;
+          break;
+        case 713:
+          channel = bot.config.BLOG_CHANNEL_EN;
+          url = `https://pegasus-web.com/pegasus-spiele-blog/${slugify(title)}`;
+          message = `We've added a new article on our blog 📄  Check it out! ${url}`;
           break;
         default:
           continue;
       }
 
       const guild = bot.guilds.cache.get(bot.config.guildId);
-      await guild.channels.cache.get(bot.config.BLOG_CHANNEL).send(message);
+      await guild.channels.cache.get(channel).send(message);
 
       const blogPost = new BlogModel();
       blogPost.blogPost_id = entry.id;
@@ -48,7 +60,7 @@ exports.run = async (bot) => {
       blogPost.url = url;
       await blogPost.save();
     } catch (error) {
-      continue;
+      console.error(errror);
     }
   }
 };
