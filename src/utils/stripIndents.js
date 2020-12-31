b09526a80c4522d629bd4ce2f100742943800e17
Gruselhaus <nico@gruselhaus.com>
2020-12-31 14:05:42 +0100

    License updates

--- src/utils/stripIndents.js
+++ src/utils/stripIndents.js
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2020 - 2021 Pegasus Spiele Verlags- und Medienvertriebsgesellschaft mbH, all rights reserved.
+ * Copyright (c) 2020 - 2021 The Pegabot authors
+ * This code is licensed under MIT license (see LICENSE for details)
  */
 
 exports.module = (string) => string.replace(/^[ \\t]+/gm, "");
