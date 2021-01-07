94f2ca3ad42bc570687a008ca3ba6e894c12f82a
Gruselhaus <nico@gruselhaus.com>
2021-01-07 07:42:05 +0100

    Adding an env to ignore channels now for the messageDelete event

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -8,6 +8,9 @@ const { MessageEmbed } = require("discord.js");
 exports.run = async (bot, msg) => {
   if (msg.channel.id === bot.config.adminChannel) return;
   if (msg.author.id === bot.user.id) return;
+  if (bot.config.ignoredChannels) {
+    if (bot.config.ignoredChannels.split(",").includes(msg.channel.id)) return;
+  }
 
   const embed = new MessageEmbed().setDescription(`Neue gelöschte Nachricht von ${msg.member} in ${msg.channel}`).addField("Inhalt der gelöschten Nachricht", msg.content ? msg.content : "Diese Nachricht hatte keinen Inhalt.");
 
