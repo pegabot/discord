e3ecc753b29209fd17a739429ef9943e55c77c43
Gruselhaus <nico@gruselhaus.com>
2021-02-07 14:19:18 +0100

    Performance improvements on the roles reset job

--- src/jobs/resetRoles.js
+++ src/jobs/resetRoles.js
@@ -3,19 +3,22 @@
  * This code is licensed under MIT license (see LICENSE for details)
  */
 
-exports.execute = async (bot) => {
+exports.execute = (bot) => {
   const userGivenRolesModel = bot.db.model("userGivenRoles");
 
-  const entries = await userGivenRolesModel.find({ expires: { $lt: Date.now() } });
-  entries.forEach(async (entry) => {
-    const { roleId, userId } = entry;
-    const guild = bot.guilds.cache.get(bot.config.guildId);
-    const memberCache = guild.members.cache;
-    const member = memberCache.find((member) => member.id === userId);
-    if (member.roles.cache.has(roleId)) {
-      member.roles.remove(roleId);
-    }
-    entry.remove();
+  userGivenRolesModel.find({ expires: { $lt: Date.now() } }, (error, entries) => {
+    if (error) return;
+
+    entries.forEach(async (entry) => {
+      const { roleId, userId } = entry;
+      const guild = bot.guilds.cache.get(bot.config.guildId);
+      const memberCache = guild.members.cache;
+      const member = memberCache.find((member) => member.id === userId);
+      if (member.roles.cache.has(roleId)) {
+        member.roles.remove(roleId);
+      }
+      entry.remove();
+    });
   });
 };
 
