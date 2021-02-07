6ce33dde943204b951760e5129d4642ee080ad1d
Gruselhaus <nico@gruselhaus.com>
2021-02-07 15:01:31 +0100

    Adding channelCreate event

---
+++ src/events/channelCreate.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2020 - 2021 The Pegabot authors
+ * This code is licensed under MIT license (see LICENSE for details)
+ */
+
+exports.execute = async (bot, channel) => {
+  bot.logger.admin_green(`:inbox_tray: Der Kanal: ${channel} **wurde erstellt**.`);
+};
