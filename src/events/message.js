e484aa7ecd7d06d678e33cf3f4a29b0b017c7456
Gruselhaus <nico@gruselhaus.com>
2021-02-08 11:03:14 +0100

    Pay attention to ignored channels for the leveling system

--- src/events/message.js
+++ src/events/message.js
@@ -10,7 +10,9 @@ exports.execute = async (bot, message) => {
 
   if (!message.guild) return;
   if (message.author.bot) return;
-
+  if (bot.config.ignoredChannels) {
+    if (bot.config.ignoredChannels.split(",").includes(message.channel.id)) return;
+  }
   const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
   const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
   if (hasLeveledUp) {
