bf1c70c8f393dc25cb3f4e74790cd01fdbdd2aa0
Gruselhaus <nico@gruselhaus.com>
2021-02-08 16:02:03 +0100

    The bot is able to ignore complete categories

--- src/events/message.js
+++ src/events/message.js
@@ -6,17 +6,24 @@
 const Levels = require("discord-xp");
 
 exports.execute = async (bot, message) => {
-  bot.commands.handleCommand(message);
-
   if (!message.guild) return;
   if (message.author.bot) return;
   if (bot.config.ignoredChannels) {
     if (bot.config.ignoredChannels.split(",").includes(message.channel.id)) return;
   }
+
+  if (bot.config.ignoredCategories) {
+    if (message.channel.parentID) {
+      if (bot.config.ignoredCategories.split(",").includes(message.channel.parentID)) return;
+    }
+  }
+
   const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
   const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
   if (hasLeveledUp) {
     const user = await Levels.fetch(message.author.id, message.guild.id);
     message.channel.send(`${message.author}, glückwunsch! Du hast Level **${user.level}** erreicht. :tada:`);
   }
+
+  bot.commands.handleCommand(message);
 };
