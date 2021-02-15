13a830a6e004fd617190ef4e42d257c7bf9b1994
Gruselhaus <nico@gruselhaus.com>
2021-02-15 01:09:52 +0100

    Delete the corrosponding level document when a member leaves the guild

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -6,6 +6,14 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.execute = (bot, member) => {
+  const LevelsModel = bot.db.model("levels");
+  LevelsModel.find({ userID: member.user.id }, (error, data) => {
+    if (error) return;
+    data.forEach((element) => {
+      element.remove();
+    });
+  });
+
   const embed = new MessageEmbed().setTitle(`${member.user.tag} hat gerade den Server verlassen.`);
 
   bot.channels.resolve(bot.config.goodbyeChannel).send(embed);
