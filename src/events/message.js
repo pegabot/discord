4d7783508f2159c2bbee717fbf2f394c32961dd9
Nico Finkernagel <nico@gruselhaus.com>
2021-03-12 20:01:36 +0100

    Fixes #103

--- src/events/message.js
+++ src/events/message.js
@@ -18,7 +18,7 @@ exports.execute = async (bot, message) => {
     }
   }
 
-  if (message.content.match(/(\/r\s?.*|\/roll\s?.*)/)) return message.reply(`bitte verwende \`${bot.config.prefix}roll\` oder \`${bot.config.prefix}r\`!`);
+  if (message.content.match(/^(\/r\s?.*|\/roll\s?.*)/)) return message.reply(`bitte verwende \`${bot.config.prefix}roll\` oder \`${bot.config.prefix}r\`!`);
 
   if (!message.content.startsWith(bot.config.prefix)) {
     const MessageModel = bot.db.model("message");
