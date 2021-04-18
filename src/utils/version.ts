740419192366d8e3ed440ea29bc8ab87411bebc1
Gruselhaus <nico@gruselhaus.com>
2021-04-18 12:07:47 +0200

    Restoring the correct version.ts

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,1 +1,7 @@
-export const version = '2.0.7';
+/*
+ * Copyright (c) 2020 - 2021 The Pegabot authors
+ * This code is licensed under MIT license (see LICENSE for details)
+ */
+
+// Gets replaced on build by CI
+export const version = "x.y.z";
