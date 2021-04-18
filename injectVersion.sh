59c19418feda3f4ffc7d9691c6e7b3eace980cac
Gruselhaus <nico@gruselhaus.com>
2021-04-18 12:06:24 +0200

    ✅ Fix: removing the trailing whitespace from the Version

--- injectVersion.sh
+++ injectVersion.sh
@@ -3,6 +3,15 @@
 # Copyright (c) 2020 - 2021 The Pegabot authors
 # This code is licensed under MIT license (see LICENSE for details)
 
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   
+    printf '%s' "$var"
+}
+
 file="src/utils/version.ts"
 
 if [ -f $file ]
@@ -16,4 +25,4 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-echo "export const version = '$PACKAGE_VERSION';"  > $file
+echo "export const version = '$(trim $PACKAGE_VERSION)';"  > $file
--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,7 +1,1 @@
-/*
- * Copyright (c) 2020 - 2021 The Pegabot authors
- * This code is licensed under MIT license (see LICENSE for details)
- */
-
-// Gets replaced on build by CI
-export const version = "x.y.z";
+export const version = '2.0.7';
