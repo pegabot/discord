f16f7eb520730f4bfdcd31288a826f0a9134cfd2
Gruselhaus <nico@gruselhaus.com>
2021-04-28 22:32:40 +0200

    Adding an event handler for the channelUpdate event

---
+++ src/events/channelUpdate.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 - 2021 The Pegabot authors
+ * This code is licensed under GNU Affero General Public License v3.0
+ * (see https://github.com/pegabot/discord/blob/main/LICENSE for details)
+ */
+
+import { Channel, GuildChannel } from "discord.js";
+import { Event } from "../classes/event";
+
+export class channelUpdateEvent extends Event {
+  execute(oldChannel: Channel, newChannel: Channel): void {
+    if (oldChannel.type === "dm" || newChannel.type === "dm") return;
+
+    oldChannel.fetch();
+    newChannel.fetch();
+
+    let oldGuildChannel: GuildChannel = oldChannel as GuildChannel;
+    let newGuildChannel: GuildChannel = newChannel as GuildChannel;
+
+    if (oldGuildChannel.name !== newGuildChannel.name) {
+      this.bot.logger.admin_blue(`Der Kanal \`${oldGuildChannel.name}\` heißt jetzt \`${newGuildChannel.name}\``);
+    }
+
+    if (oldGuildChannel.parent !== newGuildChannel.parent) {
+      this.bot.logger.admin_blue(
+        `Der Kanal \`${newGuildChannel.name}\` wurde von \`${oldGuildChannel.parent?.name || "keiner Kategorie"}\` zu \`${
+          newGuildChannel.parent?.name || "keiner Kategorie"
+        }\` verschoben`,
+      );
+    }
+  }
+}
