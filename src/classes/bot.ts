bb179f60c4c5c40052112b1db92250d563dfac3d
Gruselhaus <nico@gruselhaus.com>
2021-04-29 17:28:24 +0200

    Improving the bot destroys method

--- src/classes/bot.ts
+++ src/classes/bot.ts
@@ -35,8 +35,8 @@ export class Bot {
     this.events.loadEvents();
   }
 
-  public destroy(signal: NodeJS.Signals): void {
-    console.log(`${signal} recieved, destroying the bot.`);
+  public destroy(signal?: NodeJS.Signals): void {
+    console.log(`${signal || "Signal"} recieved, destroying the bot.`);
     this.twitchClient.close();
     this.client.destroy();
     this.db?.disconnect();
