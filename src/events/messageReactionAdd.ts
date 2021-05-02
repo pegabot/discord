pegabot/discord

commit 602f21ff33cc87d302c8f9f66c67f25a9b0d2dda

Handling command recalls

src/events/messageReactionAdd.ts
@@ -29,7 +29,9 @@ export class messageReactionAddEvent extends Event {
 
     switch (reaction.emoji.name) {
       case emojis.rollEmoji:
-        this.bot.client.emit("handleReroll", user, reaction);
+        this.bot.client.emit("handleReroll", reaction, user);
+      case emojis.commandReactionEmoji:
+        this.bot.client.emit("handleCommandRecall", reaction, user);
       default:
         return;
     }

src/events/handleCommandRecall.ts
@@ -1,0 +1,18 @@
+/*
+ * Copyright (c) 2020 - 2021 The Pegabot authors
+ * This code is licensed under GNU Affero General Public License v3.0
+ * (see https://github.com/pegabot/discord/blob/main/LICENSE for details)
+ */
+
+import { MessageReaction, User } from "discord.js";
+import { Event } from "../classes/event";
+
+export class handleCommandRecallEvent extends Event {
+  async execute(reaction: MessageReaction, user: User): Promise<void> {
+    if (reaction.message.member?.id !== user.id) {
+      reaction.message.channel.send(`${user} nur der ursprüngliche Autor kann einen Command erneut ausführen.`);
+      return;
+    }
+    this.bot.commands.handleCommand(reaction.message);
+  }
+}

src/events/handleReroll.ts
@@ -12,7 +12,7 @@ import { fetchWithTimeout } from "../utils/fetchWithTimeout";
 import { generateEmbed, generateParams, rollDice } from "../utils/RollButler";
 
 export class handleRerollEvent extends Event {
-  async execute(user: User, reaction: MessageReaction): Promise<void> {
+  async execute(reaction: MessageReaction, user: User): Promise<void> {
     const {
       message: { id: messageID },
     } = reaction;

commit 62ef6f1db72a436da090506b43c4621e350b9ffd

Fixing a bug introduced in 602f21ff33cc87d302c8f9f66c67f25a9b0d2dda

src/events/messageReactionAdd.ts
@@ -30,8 +30,10 @@ export class messageReactionAddEvent extends Event {
     switch (reaction.emoji.name) {
       case emojis.rollEmoji:
         this.bot.client.emit("handleReroll", reaction, user);
+        break;
       case emojis.commandReactionEmoji:
         this.bot.client.emit("handleCommandRecall", reaction, user);
+        break;
       default:
         return;
     }