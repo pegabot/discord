c0bb097f52091f3fce6442af96fbd6d041588466
Gruselhaus <nico@gruselhaus.com>
2021-05-06 23:40:14 +0200

    improved prune command

--- src/commands/moderation/prune.ts
+++ src/commands/moderation/prune.ts
@@ -10,29 +10,37 @@ import { CommandExecption } from "../../utils/execptions";
 
 export class PruneCommand extends Command {
   name = "prune";
-  usage = "prune <Anzahl der zu löschenden Nachrichten>";
+  usage = "prune <Anzahl der zu löschenden Nachrichten (Dies ist optional, der Standartwert ist 100)>";
   help = "Löscht eine gewisse Anzahl an Nachrichten";
   permissions = ["MANAGE_MESSAGES"];
   repeatable = false;
 
+  async fallbackMethod(msg: Message, numberOfMessageToDelete: number) {
+    console.log("Using fallback");
+
+    const messages = await msg.channel.messages.fetch({ limit: numberOfMessageToDelete + 1 });
+
+    for (const msgToDelete of messages.values()) {
+      if (msgToDelete.deletable) {
+        msgToDelete.delete({ reason: this.bot.client.user?.id });
+      } else {
+        msg.channel.send(`Die folgende Nachricht konnte von mir nicht gelöscht werden\n>>> ${msgToDelete.content}`);
+      }
+    }
+  }
+
   async execute(msg: Message, args: string[]) {
     if (args[0]) {
       if (isNaN(Number(args[0]))) throw new CommandExecption("Dein übergebener Wert ist keine Zahl.");
-      if (Number(args[0]) > 50) throw new CommandExecption("Ich kann nicht mehr als 50 Nachrichten auf Einmal löschen.");
-
-      const messages = await msg.channel.messages.fetch({ limit: Number(args[0]) + 1 });
-
-      for (const msgToDelete of messages.values()) {
-        if (msgToDelete.deletable) {
-          msgToDelete.delete();
-        } else {
-          msg.channel.send(`Die folgende Nachricht konnte von mir nicht gelöscht werden\n>>> ${msgToDelete.content}`);
-        }
-      }
-
-      return;
+      if (Number(args[0]) > 99) throw new CommandExecption("Ich kann nicht mehr als 99 Nachrichten auf Einmal löschen.");
     }
 
-    (msg.channel as TextChannel).bulkDelete(args[0] ? Number(args[0]) + 1 : 100);
+    const numberOfMessageToDelete = Number(args[0]) || 100;
+
+    try {
+      await (msg.channel as TextChannel).bulkDelete(numberOfMessageToDelete + 1);
+    } catch (error) {
+      this.fallbackMethod(msg, numberOfMessageToDelete);
+    }
   }
 }
