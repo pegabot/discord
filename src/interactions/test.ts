e72e5c6563f4853756bfa7294745462e1f644a18
Gruselhaus <nico@gruselhaus.com>
2021-05-08 12:51:56 +0200

    removing test.ts

--- src/interactions/test.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationCommandOptionData } from "discord.js";
-import { InteractionCommand } from "../core/interactions/interactionCommand";
-
-export class TestInteraction extends InteractionCommand {
-  name = "main";
-  description = "test";
-  options: ApplicationCommandOptionData[] = [{ name: "add", type: "SUB_COMMAND", description: "test" }];
-  async execute(): Promise<void> {}
-}
